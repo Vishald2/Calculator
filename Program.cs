6a2daae0f6a906d5c4c385a113212bd63c69350e
vishal <vishald24@yahoo.com>
2020-01-16 22:10:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enetr your name!");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadKey();
+           
+        }
+    }
+}
